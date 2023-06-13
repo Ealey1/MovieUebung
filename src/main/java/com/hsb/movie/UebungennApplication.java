bf720a8e382db1291ba97afa10bc86e611298756
Denis <denis.karlinski@gmail.com>
2023-06-13 22:29:18 +0200

    a

--- src/main/java/com/hsb/movie/UebungennApplication.java
+++ src/main/java/com/hsb/movie/UebungennApplication.java
@@ -1,13 +1,30 @@
 package com.hsb.movie;
 
+import com.hsb.movie.movies.Movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class UebungennApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UebungennApplication.class, args);
 	}
 
+
+	@GetMapping(path = "/movies")
+	//es funktioniert so alles. kp was der default path ist und wie es ohne path funktioniert hätte
+	public List<Movie> movieList(){
+		return List.of(
+				new Movie(
+						"Prey"
+				)
+		);
+
+	};
 }
--- src/main/java/com/hsb/movie/movies/Movie.java
+++ src/main/java/com/hsb/movie/movies/Movie.java
@@ -1,2 +1,18 @@
-package com.hsb.movie.movies;public class Movie {
+package com.hsb.movie.movies;
+
+public class Movie {
+
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
